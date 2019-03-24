c807fc18e12210ec078a2dd1b395ebcf51db4323
romgu <gmarca20@gmail.com>
2019-03-24 13:47:33 -0300

    Updates

---
+++ precache-manifest.703841f120008f1b0a5fd7be54b05cb2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c62aded729bf7146d491275e5019d7fc",
+    "url": "/guillermoroman/static/media/Gilroy-Light.c62aded7.otf"
+  },
+  {
+    "revision": "0f6e082f42f5247d82c3ade9c2a8f928",
+    "url": "/guillermoroman/static/media/Gilroy-ExtraBold.0f6e082f.otf"
+  },
+  {
+    "revision": "2d56b3e479787473f71a",
+    "url": "/guillermoroman/static/js/runtime~main.3af3eb3e.js"
+  },
+  {
+    "revision": "5c07408d7766d8876401",
+    "url": "/guillermoroman/static/js/main.b38ab998.chunk.js"
+  },
+  {
+    "revision": "bae642c1f1670353688f",
+    "url": "/guillermoroman/static/js/2.e520c56b.chunk.js"
+  },
+  {
+    "revision": "5c07408d7766d8876401",
+    "url": "/guillermoroman/static/css/main.9b221935.chunk.css"
+  },
+  {
+    "revision": "bae642c1f1670353688f",
+    "url": "/guillermoroman/static/css/2.c212a55c.chunk.css"
+  },
+  {
+    "revision": "099ccf8098696bbec2ef10f76a8494b1",
+    "url": "/guillermoroman/index.html"
+  }
+];
